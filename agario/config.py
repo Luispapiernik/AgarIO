589ebcda616b4e796f34ceea06b7bc1acd891e29
Luis Papiernik <lpapiernik24@gmail.com>
2022-07-13 17:16:03 -0500

    the game manager was added

--- agario/config.py
+++ agario/config.py
@@ -1,8 +1,14 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    ...
+    width: int = Field(None, env="WIDTH")
+    height: int = Field(None, env="HEIGHT")
+    title: str = Field(None, env="TITLE")
+
+    class Config:
+        env_file = "settings.env"
+        env_file_encoding = "utf-8"
 
 
 settings = Settings()
--- agario/gamemanager.py
+++ agario/gamemanager.py
@@ -1,3 +1,26 @@
+import pygame as pg
+
+from agario.config import settings
+
+pg.display.init()
+
+
 class GameManager:
+    def __init__(self) -> None:
+        self.width = settings.width
+        self.height = settings.height
+
+        pg.display.set_caption(settings.title)
+        self.screen = pg.display.set_mode((self.width, self.height))
+
+        self.quit = False
+
     def run(self):
-        pass
+        while not self.quit:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.quit = True
+                if event.type == pg.KEYDOWN and event.key == pg.K_q:
+                    self.quit = True
+
+        pg.display.quit()
